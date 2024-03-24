83c57f7dc689180cb359e6c2882028257d9245df
Blackbird-3 <agarwalshreshth3@gmail.com>
2024-03-24 23:02:16 +0530

    before upgrade

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:sound_slice/util/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webscreenSize) {
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
---
+++ lib/util/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(129, 210, 199, 1);
